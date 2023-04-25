215725ab66075caf0c33fe502cad721bfd362dc1
nramirezjclds <nramirezjclds@yahoo.com>
2023-04-24 22:23:34 -0500

    Week 2 activities

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number: ");
+            string response = Console.ReadLine();
+            userNumber = int.Parse(response);
+        
+            if (userNumber != 0)
+                {
+                    numbers.Add(userNumber);
+                }
+        }
+
+        //sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        //average
+        float average = ((float)sum / numbers.Count);
+        Console.WriteLine($"The average is: {average}");
+
+        //max
+        int max = numbers [0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
